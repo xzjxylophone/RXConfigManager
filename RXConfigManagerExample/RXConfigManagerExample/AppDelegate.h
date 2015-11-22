284153bb140f6c4477ed6b247313cc3d5ddb583c
Rush.D.Xzj <xzjxylophone@gmail.com>
2015-11-22 11:27:55 +0800

    Add

---
+++ RXConfigManagerExample/RXConfigManagerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RXConfigManagerExample
+//
+//  Created by Rush.D.Xzj on 15/11/22.
+//  Copyright © 2015年 Rush.D.Xzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RXConfigManagerExample/RXConfigManagerExample/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  RXConfigManagerExample
+//
+//  Created by Rush.D.Xzj on 15/11/22.
+//  Copyright © 2015年 Rush.D.Xzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
